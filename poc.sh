14a577f31634e51dc4fa9857b3552ffa1f1761d4
bawey <tomek.bawej@gmail.com>
2023-08-16 13:00:36 +0200

    poc.sh: channel switching and curl retries

--- poc.sh
+++ poc.sh
@@ -3,12 +3,22 @@
 # A crude proof of concept
 
 HOST='ddh1.cdndac.lol'
-ROOT="https://$HOST/ddh1/premium75/tracks-v1a1"
+CHANNEL="${1:-75}"
+ROOT="https://$HOST/ddh1/premium$CHANNEL/tracks-v1a1"
 LIST='mono.m3u8'
 ORIGIN='https://superwebplay.xyz/'
+OUTPUT_FILE="/tmp/dumped-stream-$CHANNEL.ts"
 
 # For a brute-force channels scan, do:
-#for i in {01..99}; do URL="${ROOT/75/$i}/$LIST"; echo $URL; curl $URL --output -; done
+#for i in {01..99}
+#do
+#  URL="${ROOT/75/$i}/$LIST";
+#  PLAYLIST=$(curl $URL -s --output -);
+#  if [[ $PLAYLIST =~ ".ts" ]]
+#  then
+#     echo $URL;
+#  fi
+#done
 
 
 ALL_SEGMENTS=()
@@ -16,20 +26,20 @@ while :
 do
   CHUNKS=''
   ((REPEATED_COUNT=0))
-  for SEG in $(curl "$ROOT/$LIST" -H "Origin: $ORIGIN"  -H "Host: $HOST" -H "Referer: $ORIGIN" --output - | grep ".ts"); do
+  for SEG in $(curl --retry 3 "$ROOT/$LIST" -H "Origin: $ORIGIN"  -H "Host: $HOST" -H "Referer: $ORIGIN" --output - | grep ".ts"); do
     if [[ ${ALL_SEGMENTS[@]} =~ $SEG ]]
     then
       echo "segment $SEG seen already"
       ((REPEATED_COUNT+=1))
     else
-      CHUNK=$(curl -L -m 3 "$ROOT/$SEG" -H "Origin: $ORIGIN" -H "Referer: $ORIGIN" -H "Host: $HOST" --output - | base64)
+      CHUNK=$(curl -L -m 5 --retry 3 --retry-all-errors "$ROOT/$SEG" -H "Origin: $ORIGIN" -H "Referer: $ORIGIN" -H "Host: $HOST" --output - | base64)
       ALL_SEGMENTS+=("$SEG")
       CHUNKS="$CHUNKS$CHUNK"
     fi
   done
   #To watch each fetched piece as soon as it lands:
   #mpv <(echo "$CHUNKS" | base64 -d) &
-  OUTPUT_FILE="/tmp/dumped-stream.ts"
+
   echo "$CHUNKS" | base64 -d >> $OUTPUT_FILE
   if [[ $(pgrep -fa $OUTPUT_FILE) =~ $OUTPUT_FILE ]]
   then
